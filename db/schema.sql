dfebbc8dd0760e550121d743888f7bc76591e548
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2020-02-11 18:06:34 -0500

    Added schema and seed sql code

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN,
+    PRIMARY KEY (id) 
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES 
+("Classic Cheeseburger", 0),
+("Bacon Cheeseburger", 0),
+("Double Cheeseburger", 0);
